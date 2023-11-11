67931bf0aafc76f93c3878c3b4d4714f17e00538
Muhammad Sohaib Ali <sohaibayub9@gmail.com>
2023-11-11 13:51:49 +0500

    updated assert

--- tests/test_home.py
+++ tests/test_home.py
@@ -3,7 +3,7 @@ import unittest
 class TestApp(unittest.TestCase):
     def test_home_200(self):
         response = 400
-        assert 200 == 200
+        assert response == 200
 
 if __name__ == "__main__":
     unittest.main()
